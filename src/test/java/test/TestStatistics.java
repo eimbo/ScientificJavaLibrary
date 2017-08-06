f327c609338baf7513cdcccab58b85d26ff485cf
Jesse Payton <jepayton12013@gmail.com>
2017-08-06 14:24:10 -0400

    update testing

--- src/test/java/test/TestStatistics.java
+++ src/test/java/test/TestStatistics.java
@@ -1,13 +1,13 @@
-package scientific_java;
+package test;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for Statistics.java.
  */
-public class AppTest 
+public class TestStatistics 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public TestStatistics( String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( TestStatistics.class );
     }
 
     /**
