69ab94acc991c4bcc6fb681cc67b754ed933d1cb
Jelle Hulter <jellehulter@gmail.com>
2019-01-24 23:38:15 +0100

    Added initiator functions for Matrix class.

--- src/main/java/scientific/Matrix.java
+++ src/main/java/scientific/Matrix.java
@@ -9,6 +9,40 @@ package scientific;
 
 
 public class Matrix {
-	// TODO implement
-	
+
+	private final int width, height;
+	private int[][] matrix;
+
+	public Matrix(int width, int height){
+		this.width = width;
+		this.height = height;
+		matrix = new int[height][width];
+	}
+
+	public Matrix(int[][] matrix){
+		this.matrix = matrix;
+		this.height = matrix.length;
+		this.width = matrix[0].length;
+	}
+
+	public Matrix setMatrix(int[][] matrix){
+		if(matrix.length != height || matrix[0].length != width)
+			return null;
+
+		for(int x = 0; x < width; x++){
+			for(int y = 0; y < height; y++){
+				this.matrix[y][x] = matrix[y][x];
+			}
+		}
+		return this;
+	}
+
+	public int getIndex(int x, int y){
+		return matrix[y][x];
+	}
+
+	public void setIndex(int x, int y, int value){
+		matrix[y][x] = value;
+	}
+
 }
