ac3df9cb00cd9c21c9f21df6ad155f39cbb8d8c6
ele-ra <63599689+ele-ra@users.noreply.github.com>
2022-11-14 03:16:22 +0300

    Tests for Complex class (#19)
    approved

---
+++ src/test/java/tests/ComplexTest.java
@@ -0,0 +1,76 @@
+package tests;
+
+import org.junit.Test;
+import scientific.Complex;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ComplexTest {
+
+    @Test
+    public void correctlyAssignsReAndImParts() {
+        Complex value = new Complex(2,4);
+        assertEquals(2,value.real(), 0);
+        assertEquals(4,value.imaginary(), 0);
+    }
+
+    @Test
+    public void correctyShowsValueWithNegativeImAsAString() {
+        Complex value = new Complex(2,-4);
+        assertEquals("2.0 - 4.0i", value.toString());
+    }
+
+    @Test
+    public void correctyShowsValueWithPositiveImAsAString() {
+        Complex value = new Complex(2,4);
+        assertEquals("2.0 + 4.0i", value.toString());
+    }
+
+    @Test
+    public void correctySumsValues() {
+        Complex value1 = new Complex(2,4);
+        Complex value2 = new Complex(4,6);
+        assertEquals(new Complex(6,10), value1.plus(value2));
+    }
+
+    @Test
+    public void correctySubtractsValues() {
+        Complex value1 = new Complex(2,4);
+        Complex value2 = new Complex(4,6);
+        assertEquals(new Complex(-2,-2), value1.minus(value2));
+    }
+
+    @Test
+    public void correctyTimesValues() {
+        Complex value1 = new Complex(2,4);
+        Complex value2 = new Complex(4,6);
+        assertEquals(new Complex(-16,28), value1.times(value2));
+    }
+
+    @Test
+    public void correctyScalesValue() {
+        Complex value = new Complex(2,4);
+        assertEquals(new Complex(4,8), value.scale(2));
+    }
+
+    @Test
+    public void correctConjugatesValue() {
+        Complex value = new Complex(2,4);
+        assertEquals(new Complex(2,-4), value.conjugate());
+    }
+
+    @Test
+    public void correctReciprocalsValue() {
+        Complex value = new Complex(2,4);
+        assertEquals(new Complex(0.1,-0.2), value.reciprocal());
+    }
+
+    @Test
+    public void correctDividesValues() {
+        Complex value1 = new Complex(2,4);
+        Complex value2 = new Complex(4,6);
+        assertEquals(0.6154, value1.divides(value2).real(), 0.1);
+        assertEquals(0.0769, value1.divides(value2).imaginary(), 0.1);
+    }
+}
